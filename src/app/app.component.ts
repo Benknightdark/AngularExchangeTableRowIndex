a58da454f4e65f3c8ed36732c37b83417c20e1ed
Benknightdark <moon791017@gmail.com>
2017-12-09 17:40:49 +0800

    OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "app";
+  RowData;
+  ngOnInit() {
+    this.RowData = [
+      { FirstName: "Jill1", LastName: "Huo1", Age: 50 },
+      { FirstName: "Jill2", LastName: "Huo2", Age: 770 },
+      { FirstName: "Jill3", LastName: "Huo3", Age: 44 },
+      { FirstName: "Jill4", LastName: "Huo4", Age: 33 },
+      { FirstName: "Jill5", LastName: "Huo5", Age: 22 },
+      { FirstName: "Jill6", LastName: "Huo6", Age: 11 }
+    ];
+  }
+  RowUp(index,SelectedData){
+    if(index>0){
+      const PrevIndex=index-1
+      const PrevData=this.RowData[PrevIndex]
+      this.RowData[index]=PrevData;
+      this.RowData[PrevIndex]=SelectedData;
+    }
+
+  }
+  RowDown(index,SelectedData){
+    if(index<this.RowData.length-1){
+      const NextIndex=index+1
+      const PrevData=this.RowData[NextIndex]
+      this.RowData[index]=PrevData;
+      this.RowData[NextIndex]=SelectedData;
+    }
+
+  }
 }
